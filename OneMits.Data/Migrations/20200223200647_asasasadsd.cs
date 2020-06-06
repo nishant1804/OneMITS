a8ab12f5002e87713b7db268543f06f1dd3e89f5
Nishant Raghuvanshi <nishant4337@gmail.com>
2020-06-06 22:26:18 +0530

    Commit All

---
+++ OneMits.Data/Migrations/20200223200647_asasasadsd.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OneMits.Data.Migrations
+{
+    public partial class asasasadsd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
